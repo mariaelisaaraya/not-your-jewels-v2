34c2a8e9e4f6b9338e48b66ef57da0e805cef5d3
mariaelisaaraya <maria.elisa.araya.work@gmail.com>
2025-08-31 06:47:55 -0300

    update next config

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   devIndicators: false,
   images: {
-  domains: ['images.pexels.com'],
+    domains: ["images.pexels.com"],
   },
   typescript: {
     ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
