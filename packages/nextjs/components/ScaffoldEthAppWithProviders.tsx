d7eda0f55ca03e3908de34952c47e4f22a90cc4c
mariaelisaaraya <maria.elisa.araya.work@gmail.com>
2025-08-31 06:28:36 -0300

    feat: add scaffoldethappwhitproviders

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -8,10 +8,23 @@ import { useTheme } from "next-themes";
 import { Toaster } from "react-hot-toast";
 import { WagmiProvider } from "wagmi";
 import { Footer } from "~~/components/Footer";
-import { Header } from "~~/components/Header";
+//import { Header } from "~~/components/Header";
 import { BlockieAvatar } from "~~/components/scaffold-eth";
 import { useInitializeNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
 import { wagmiConfig } from "~~/services/web3/wagmiConfig";
+
+import { createStorage } from "wagmi";
+
+const customWagmiConfig = {
+  ...wagmiConfig,
+  storage: createStorage({
+    storage: {
+      getItem: () => null,        
+      setItem: () => {},         
+      removeItem: () => {},       
+    },
+  }),
+};
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   useInitializeNativeCurrencyPrice();
@@ -19,7 +32,7 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <div className={`flex flex-col min-h-screen `}>
-        <Header />
+        {/* <Header /> */}
         <main className="relative flex flex-col flex-1">{children}</main>
         <Footer />
       </div>
@@ -46,7 +59,7 @@ export const ScaffoldEthAppWithProviders = ({ children }: { children: React.Reac
   }, []);
 
   return (
-    <WagmiProvider config={wagmiConfig}>
+    <WagmiProvider config={customWagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider
           avatar={BlockieAvatar}
